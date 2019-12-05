7862d10dad733179a29802b5ebb377beb7698edc
yujiw001 <wany5d1401@gmail.com>
2019-12-04 18:23:22 -0800

    insersion successful

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-// const bodyparser = require('body-parser');
-// app.use(bodyparser.json);
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+
 //Create connection
 const target =3;
 const trash =3;
@@ -20,6 +21,8 @@ db.connect((err) => {
     }
     console.log('MySql Connected...')
 });
+var cors = require('cors');
+app.use(cors());
 //增
 
 // let post = {id:1, lan_mark:'E', area:'LA',department:'tech',position:'Software developer',brief:'We are looking for an Intermediate Software Engineer to join our dev team during a dynamic growth stage of the company. We develop web-based software for TMS(transportation management system) , EDI and some portals. We are seeking an individual with a positive attitude with skills to match, being self-motivated and striving for excellence in their work, willing to grow and learn in our dynamic environment and the ability to work independently and within a team. So you will be having lots of opportunities to learn tech skills on Asp.net Framework/Core in Microsoft Azure Cloud.',responsibilities:' ',requirements:'',expedited:false};
@@ -49,9 +52,10 @@ db.connect((err) => {
 // })
 
 //增加新用户
-app.get('/users/add', (req,res)=>{
-    const {username,password} = req.query;
-    const INSERT_USERS_QUERY = `INSERT INTO employee_account (username,password) VALUES('${username}',${password})`;
+app.post('/users/add',jsonParser, (req,res)=>{
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    const {username,password} = req.body;
+    const INSERT_USERS_QUERY = `INSERT INTO employee_account (username,password) VALUES('${username}','${password}')`;
     db.query(INSERT_USERS_QUERY,(err,results)=>{
         if(err){
             return res.send(err)
@@ -61,9 +65,62 @@ app.get('/users/add', (req,res)=>{
         }
     });
 });
-
-
-
+// 增加新的司机
+app.post('/drivers/add' ,jsonParser, (req,res)=>{
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    const {First_Name,Last_Name,Mobile,Address,City,PostalCode,description} = req.body;
+    const INSERT_DRIVER_APPLICATION = `INSERT INTO driver_application (first_Name,
+        last_Name,phone,street_address,city,postal_code,self_comment) 
+        VALUES('${First_Name}','${Last_Name}','${Mobile}','${Address}','${City}','${PostalCode}','${description}')`;
+        
+    db.query(INSERT_DRIVER_APPLICATION,(err,results)=>{
+        if(err){
+            return res.send(err)
+        }
+        else{
+            return res.send('插入成功')
+        }
+    });
+});
+//增加工作时间
+// app.get('/drivers/availabletime' , (req,res)=>{
+//     const {AvailableTime} = req.query;
+//     const INSERT_AVAILABLETIME = `INSERT INTO available_work_time (available_time) VALUSES('${AvailableTime}')`;
+//     db.query(INSERT_AVAILABLETIME,(err,results)=>{
+//         if(err){
+//             return res.send(err)
+//         }
+//         else{
+//             return res.send('插入成功')
+//         }
+//     });
+// })
+//增加意向地区
+// app.get('/drivers/desiredarea' , (req,res)=>{
+//     const {DesiredArea} = req.query;
+//     const INSERT_DESIREAREA = `INSERT INTO desired_area (available_area) VALUSES('${DesiredArea}')`;
+//     db.query(INSERT_DESIREAREA,(err,results)=>{
+//         if(err){
+//             return res.send(err)
+//         }
+//         else{
+//             return res.send('插入成功')
+//         }
+//     });
+// })
+//增加送餐方式
+// app.get('/drivers/transportation' , (req,res)=>{
+//     const {Transportation} = req.query;
+//     const INSERT_DESIREAREA = `INSERT INTO transportation (transportation_method) VALUSES('${Transportation}')`;
+//     db.query(INSERT_DESIREAREA,(err,results)=>{
+//         if(err){
+//             return res.send(err)
+//         }
+//         else{
+//             return res.send('插入成功')
+//         }
+//     });
+// })
 
 app.listen('3000' , () => {
     console.log('Server started on port 3000');
